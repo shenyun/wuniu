2f827b60248f1b73fb7271003b8a361a9f7d9f0e
yong.luo <yong.luo@msxf.com>
2016-08-30 11:54:13 +0800

    commons-lang3

--- wuniu-web/src/test/java/test.wuniu/TestMain.java
+++ wuniu-web/src/test/java/test.wuniu/TestMain.java
@@ -1,7 +1,11 @@
 package test.wuniu;
 
 
+import org.apache.commons.lang.math.NumberUtils;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 /**
  * Created by yong.luo on 2016/6/21.
@@ -11,5 +15,16 @@ public class TestMain {
     @Test
     public void  test() {
         System.out.println("test");
+        String a="1233";
+        System.out.println(a.toString());
+        Double dd=123.56D;
+//        Object fa=dd;
+//        Integer intt=(Integer)fa;
+        System.out.println(dd.toString());
+        if (NumberUtils.isNumber(dd.toString())) {
+            BigDecimal big=new BigDecimal(dd.toString());
+            System.out.println(big);
+        }
+
     }
 }
